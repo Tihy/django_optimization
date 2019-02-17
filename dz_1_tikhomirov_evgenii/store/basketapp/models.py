ab864df42875bebc0537a651e5fb5ef3fee026f5
Tihy <tihomirov.evgenii@gmail.com>
2019-02-17 14:47:18 +0100

    Lesson 4

--- dz_1_tikhomirov_evgenii/store/basketapp/models.py
+++ dz_1_tikhomirov_evgenii/store/basketapp/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from django.conf import settings
 from mainapp.models import Product
 
+class BasketQuerySet(models.QuerySet):
+    def delete(self, *args, **kwargs):
+        for object in self:
+            object.product.quantity += object.quantity
+            object.product.save()
+        super().delete(*args, **kwargs)
 
 class Basket(models.Model):
+    objects = BasketQuerySet.as_manager()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="baskets")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
@@ -42,5 +49,33 @@ class Basket(models.Model):
         basket_items = cls.get_items(user)
         basket_items_dic = {}
         [basket_items_dic.update({item.product: item.quantity}) for item in basket_items]
-
         return basket_items_dic
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.filter(pk=pk).first()
+
+    def save(self, *args, **kwargs):
+        # 1. Как определить сохраняем мы новый объект или изеняем старый
+        # 2. Как использовать старые данные при изменении
+
+        # 1.
+        if self.pk:
+            # Изменяем объект
+            print('Значения')
+            print('q новое значение', self.quantity)
+
+            # print('q2 старое значение', self.__class__.get_item(self.pk).quantity)
+            # old = Basket.objects.get(pk=self.pk).quantity
+            self.product.quantity -= self.quantity - self.__class__.get_item(self.pk).quantity
+        else:
+            # Это новый объект - создаем
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super().save(*args, **kwargs)
+
+    # переопределяем метод, удаляющий объект
+    def delete(self):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super().delete()
